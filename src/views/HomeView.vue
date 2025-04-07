3d1e30b933509fc2026a11a30a8ae218025b89aa
Andrew Mason <andrewmason@Andrews-MacBook-Pro.local>
2025-04-07 18:36:34 -0400

    Add overflow-x hidden, update section text body alignment and spacing mobile

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -58,7 +58,7 @@ const getImageUrlSubfolder = (subfolder, name, extension) => {
       </div>
       
       <div class="row m-0">
-          <div class="col-lg-12 px-4 pt-4 min-vh-75" :style="{'background' : 'url('+ getImageUrl('eh_blocks_shape','jpeg') +') center', 'background-size' : '100%'}">
+          <div class="col-lg-12 px-4 pt-4 min-vh-75" :style="{'background' : 'url('+ getImageUrl('eh_blocks_shape','jpeg') +') center', 'background-size' : 'cover'}">
         </div>
        
       </div>
@@ -66,16 +66,21 @@ const getImageUrlSubfolder = (subfolder, name, extension) => {
       <!-- benches -->
 
       <div id="benches" class="row m-0 pt-2">
-        <div class="col-lg-8 offset-lg-2 min-vh-75 d-flex flex-column justify-content-center my-auto align-items-center text-center p-5">
+        <div class="col-lg-8 offset-lg-2 min-vh-75 d-flex flex-column justify-content-center align-items-start align-items-lg-center text-start text-lg-center p-5">
           <h2 class="display-3 fw-900 ls-1 lh-1">Benches</h2>
           <p class="fs-5 py-3">Perfect for front door entry, hallway, mudroom, and coffee table applications, our rustic farmhouse benches are stylish and strong, built with solid Canadian wood, construction adhesive, and structural finishing screws for strength and durability. Base model is 3' long x 14" high x 15.5" deep, and can be built with custom dimensions on request. Available space underneath is 29" wide which fits a standard 28" plastic shoe mat. Comes unstained/natural finish, with a variety of stain options available.</p>
           <ButtonGroup />
         </div>
         <div class="col-lg-3 col-md-6 mb-3">
-          <img class="w-100 h-100 rounded-4" style="object-fit: cover;" :src="getImageUrlSubfolder('benches','bench2','jpg')" alt="">
+          <img class="w-100 h-100 rounded-4" style="object-fit: cover;
+  background: linear-gradient(45deg, #f4a261, transparent),
+    linear-gradient(-45deg, #e76f51, transparent),
+    linear-gradient(90deg, #8ab17d, transparent),
+    linear-gradient(0deg, #d62828, #023047);
+" :src="getImageUrlSubfolder('benches','bench2','jpg')" alt="">
         </div>
         <div class="col-lg-3 col-md-6 mb-3">
-          <img class="w-100 h-100 rounded-4" style="object-fit: cover;" :src="getImageUrlSubfolder('benches','bench3','jpg')" alt="">
+          <img class="w-100 h-100 rounded-4" style="object-fit: cover; --lqip:-776357" :src="getImageUrlSubfolder('benches','bench3','jpg')" alt="">
         </div>
         <div class="col-lg-3 col-md-6 mb-3">
           <img class="w-100 h-100 rounded-4" style="object-fit: cover;" :src="getImageUrlSubfolder('benches','bench7','jpg')" alt="">
@@ -90,7 +95,7 @@ const getImageUrlSubfolder = (subfolder, name, extension) => {
       <!-- tables -->
 
       <div id="tables" class="row m-0 pt-2">
-        <div class="col-lg-8 offset-lg-2 min-vh-75 d-flex flex-column justify-content-center my-auto align-items-center text-center p-5">
+        <div class="col-lg-8 offset-lg-2 min-vh-75 d-flex flex-column justify-content-center align-items-start align-items-lg-center text-start text-lg-center p-5">
           <h2 class="display-3 fw-900 ls-1 lh-1">Tables</h2>
           <p class="fs-5 py-3">Our handcrafted slimline console tables are perfect for narrow hallways and front entries with limited space. With room for keys, candles, vases and decorative items, they can be customized with a lower shelf for baskets and further storage. Base dimensions are 36" wide (long) x 35.5" high x 7.5" deep. Tables come unstained and can be built to order, with customizable dimensions and stain colors on request. Please inquire for more information!</p>
           <ButtonGroup />
@@ -112,7 +117,7 @@ const getImageUrlSubfolder = (subfolder, name, extension) => {
       <!-- beds -->
 
       <div id="beds" class="row m-0 pt-2">
-        <div class="col-lg-8 offset-lg-2 min-vh-75 d-flex flex-column justify-content-center my-auto align-items-center text-center p-5">
+        <div class="col-lg-8 offset-lg-2 min-vh-75 d-flex flex-column justify-content-center align-items-start align-items-lg-center text-start text-lg-center p-5">
           <h2 class="display-3 fw-900 ls-1 lh-1">Beds</h2>
           <p class="fs-5 py-3">We currently have two bed models, a house-frame style, single mattress floor bed with roofline, and a bunkbed. Both are made with construction quality lumber (2x6, 2x4, 2x3, and 1x4 knotty pine), structural fasteners and construction screws, and are built to last: think 2 boys swinging, hanging, jumping, climbing, traversing, launching, and otherwise doing their best to shred it to pieces. Frames are modular, assembled on-site, and fit standard twin mattresses. Beds come unstained but can be customized with a variety of colours and finishes.</p>
           <ButtonGroup />
@@ -134,12 +139,12 @@ const getImageUrlSubfolder = (subfolder, name, extension) => {
       <!-- blocks -->
       
       <div id="blocks" class="row m-0 pt-2">
-        <div class="col-lg-8 offset-lg-2 min-vh-75 d-flex flex-column justify-content-center my-auto align-items-center text-center p-5">
+        <div class="col-lg-8 offset-lg-2 min-vh-75 d-flex flex-column justify-content-center align-items-start align-items-lg-center text-start text-lg-center p-5">
           <h2 class="display-3 fw-900 ls-1 lh-1">Blocks</h2>
           <p class="fs-5 py-3">What began as an effort to creatively use scrap lumber grew into a fun and creative project for my own kids. Incorporating square, rectangle, triangle, column, and house shapes, larger block sizes, and natural, unfinished pine, this building block set is perfect for infants to preschoolers.</p>
           <ButtonGroup />
         </div>
-        <div class="col-lg-12 px-4 pt-4 min-vh-75" :style="{'background' : 'url('+ getImageUrl('eh_blocks_selection','jpeg') +') center', 'background-size' : '100%'}">
+        <div class="col-lg-12 px-4 pt-4 min-vh-75" :style="{'background' : 'url('+ getImageUrl('eh_blocks_selection','jpeg') +') center', 'background-size' : 'cover'}">
         </div>
       </div>
 
@@ -159,13 +164,13 @@ const getImageUrlSubfolder = (subfolder, name, extension) => {
       <!-- climbers -->
 
       <div id="climbers" class="row m-0 pt-2">
-        <div class="col-lg-8 offset-lg-2 min-vh-75 d-flex flex-column justify-content-center my-auto align-items-center text-center p-5">
+        <div class="col-lg-8 offset-lg-2 min-vh-75 d-flex flex-column justify-content-center align-items-start align-items-lg-center text-start text-lg-center p-5">
           <h2 class="display-3 fw-900 ls-1 lh-1">Climbers</h2>
           <p class="fs-5 pt-3">The <span class="fw-bold">e&h</span> kids triangle climber is designed around the famous Pikler triangle. We make a few modifications to the original design including 2x4 construction with tighter ladder/stair spacing for easier climbing and less chance of little legs getting stuck. It uses structural furniture fasteners for durability, strength, and aesthetic beauty, and pairs perfectly with the e&h climbing ramp and slide ramp add-ons. The climber is intended for supervised indoor use, however the wood can be sealed and stained* for outdoor fun.</p>
           <p class="small fst-italic mt-0 mb-3">*Be sure to use no/low-VOC and non-toxic stain/seal options for kid safety. Outdoor use exposes material to wear, rot, and rust. Always supervise children when using this product.</p> 
           <ButtonGroup />
         </div>
-        <div class="col-lg-12 px-4 pt-4 min-vh-75" :style="{'background' : 'url('+ getImageUrl('eh_pikler_closeup','jpg') +') center', 'background-size' : '100%'}">
+        <div class="col-lg-12 px-4 pt-4 min-vh-75" :style="{'background' : 'url('+ getImageUrl('eh_pikler_closeup','jpg') +') center', 'background-size' : 'cover'}">
         </div>
       </div>
 
@@ -185,13 +190,13 @@ const getImageUrlSubfolder = (subfolder, name, extension) => {
       <!-- ladder -->
 
       <div id="ladders" class="row m-0 pt-2">
-        <div class="col-lg-8 offset-lg-2 min-vh-75 d-flex flex-column justify-content-center my-auto align-items-center text-center p-5">
+        <div class="col-lg-8 offset-lg-2 min-vh-75 d-flex flex-column justify-content-center align-items-start align-items-lg-center text-start text-lg-center p-5">
           <h2 class="display-3 fw-900 ls-1 lh-1">Ladders</h2>
           <p class="fs-5 pt-3">What started as a complement/add-on to the e&h bunkbed became a standalone item. The e&h ladder is a simple and customizable unit built with dimensional 2x4s, construction screws and/or structural fasteners. It's sanded to a high natural finish and can be customized with different sizes and orientations. It can be used as a wall decoration, towel hanger, bedroom accent, attic staircase, in addition to more traditional laddery applications.</p>
           <p class="small fst-italic mt-0 mb-3">*Model shown is a 6 foot slimline 5-step variation, with 11.5" step width, 12" step spacing. Great for tight spaces, closets, bathrooms, bunkbeds, etc.</p> 
           <ButtonGroup />
         </div>
-        <div class="col-lg-12 px-4 pt-4 min-vh-75" :style="{'background' : 'url('+ getImageUrl('eh_ladder','jpeg') +') center', 'background-size' : '100%'}">
+        <div class="col-lg-12 px-4 pt-4 min-vh-75" :style="{'background' : 'url('+ getImageUrl('eh_ladder','jpeg') +') center', 'background-size' : 'cover'}">
         </div>
       </div>
 
@@ -225,7 +230,7 @@ const getImageUrlSubfolder = (subfolder, name, extension) => {
       <!-- how we work -->
 
       <div id="how-we-work" class="row m-0 pt-2">
-        <div class="col-lg-8 offset-lg-2 min-vh-75 d-flex flex-column justify-content-center my-auto align-items-center text-center p-5">
+        <div class="col-lg-8 offset-lg-2 min-vh-75 d-flex flex-column justify-content-center align-items-start align-items-lg-center text-start text-lg-center p-5">
           <h2 class="display-3 fw-900 ls-1 lh-1">How we work</h2>
           <p class="fs-5 py-3">Our products are made to order. We have varying levels of stock, but most often build on request. Timelines vary though generally take 2-4 weeks for average turnaround. At the moment we offer delivery within the Great Toronto Area and surrounding environ and ship wood blocks and giant jenga. All products are also available for pickup. Contact us for custom orders, special requests, or more information.</p>
           <TheButton
@@ -234,14 +239,14 @@ const getImageUrlSubfolder = (subfolder, name, extension) => {
               Learn more
             </TheButton>
         </div>
-        <div class="col-lg-12 px-4 pt-4 min-vh-75" :style="{'background' : 'url('+ getImageUrl('eh_blocks_selection2','jpeg') +') center', 'background-size' : '100%'}">
+        <div class="col-lg-12 px-4 pt-4 min-vh-75" :style="{'background' : 'url('+ getImageUrl('eh_blocks_selection2','jpeg') +') center', 'background-size' : 'cover'}">
         </div>
       </div>
 
       <!-- about -->
 
       <div id="about" class="row m-0 pt-2">
-        <div class="col-lg-8 offset-lg-2 min-vh-75 d-flex flex-column justify-content-center my-auto align-items-center text-center p-5">
+        <div class="col-lg-8 offset-lg-2 min-vh-75 d-flex flex-column justify-content-center align-items-start align-items-lg-center text-start text-lg-center p-5">
           <h2 class="display-3 fw-900 ls-1 lh-1">About e&h</h2>
           <p class="fs-5 py-3">Emmett & Henry is a Canadian company that builds custom furniture and wood products with precision, high quality materials, and a modern, farmhouse design aesthetic. We love what we do and are proud to create innovative, solid wood products that are safe, strong, stylish, and designed to withstand the rigors of our own family. We think they'll be great for yours too. Drop us a line for more information or to place a custom order.</p>
           <TheButton
@@ -250,7 +255,7 @@ const getImageUrlSubfolder = (subfolder, name, extension) => {
             Get in touch
           </TheButton>
         </div>
-        <div class="col-lg-12 px-4 pt-4 min-vh-75" :style="{'background' : 'url('+ getImageUrl('eh_dinos','webp') +') center', 'background-size' : '100%'}">
+        <div class="col-lg-12 px-4 pt-4 min-vh-75" :style="{'background' : 'url('+ getImageUrl('eh_dinos','webp') +') center', 'background-size' : 'cover'}">
         </div>
       </div>
 
